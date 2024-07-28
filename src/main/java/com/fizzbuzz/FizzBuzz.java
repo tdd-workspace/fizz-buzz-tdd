ed1c95370fcd6bc512905b675f3b88dd83f86254
prabanjanjeyasankar <prabanjan@gmail.com>
2024-07-28 20:12:56 +0530

    ADD. written several test cases and logic.

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,23 @@ package com.fizzbuzz;
 
 public class FizzBuzz {
     public String of(int input) {
+        String stringInput = String.valueOf(input);
+        if(input % 15 == 0){
+            return "FizzBuzz";
+        }
+        if(stringInput.contains("15")){
+            return "FizzBuzz";
+        }
         if(input % 3 == 0){
             return "Fizz";
         }
+        if(stringInput.contains("3")){
+            return "Fizz";
+        }
         if(input % 5 == 0){
+            return "Buzz";
+        }
+        if(stringInput.contains("5")){
             return "Buzz";
         }
         return String.valueOf(input);
--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -12,11 +12,27 @@ public class FizzBuzzTest {
         assertThat(fizzBuzz.of(1), is(equalTo("1")));
     }
     @Test
-    void toReturnFizzWhenTheNumberIsThreeOrItsMultiples(){
+    void toReturnFizzWhenTheNumberIsThreeOrItsMultiplesArePassed(){
         assertThat(fizzBuzz.of(3), is(equalTo("Fizz")));
     }
     @Test
-    void toReturnBuzzWhenTheNumberIsFiveOrItsMultiples(){
+    void toReturnBuzzWhenTheNumberIsFiveOrItsMultiplesArePassed(){
         assertThat(fizzBuzz.of(5), is(equalTo("Buzz")));
+    }
+    @Test
+    void toReturnFizzBuzzWhenTheNumberIsMultiplesOfThreeAndFiveArePassed(){
+        assertThat(fizzBuzz.of(15),is(equalTo("FizzBuzz")));
+    }
+    @Test
+    void toReturnFizzWhenTheNumberContainsThree(){
+        assertThat(fizzBuzz.of(130),is(equalTo("Fizz")));
+    }
+    @Test
+    void toReturnBuzzWhenTheNumberContainsFive(){
+        assertThat(fizzBuzz.of(52),is(equalTo("Buzz")));
+    }
+    @Test
+    void toReturnFizzBuzzWhenTheNumberContainsFifteen(){
+        assertThat(fizzBuzz.of(152),is(equalTo("FizzBuzz")));
     }
 }
