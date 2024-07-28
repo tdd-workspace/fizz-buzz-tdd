7d976ae2412d9159edaa12b3f0750b85ff14a84f
prabanjanjeyasankar <prabanjan@gmail.com>
2024-07-28 19:37:02 +0530

    ADD. written logic to pass the test case, code : green

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.fizzbuzz;
 
 public class FizzBuzz {
     public String of(int input) {
+        if(input == 3){
+            return "Fizz";
+        }
         return String.valueOf(input);
     }
 }
